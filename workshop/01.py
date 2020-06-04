3f64601375d478f5a1f1caac1211737c84ca3330
Nirmal <nirmal_kumar@live.in>
2020-06-04 18:03:19 +0530

    initial exercises

--- workshop/01.py
+++ workshop/01.py
@@ -3,3 +3,12 @@ print("My first training session on python")
 x=45
 
 print(">" * 45)
+
+
+
+
+print('{0:20} | {1:8} | {2:8} '.format('Apple', '25','40'))
+print('{0:20} | {1:8} | {2:8} '.format('orange', '25','40'))
+print('{0:20} | {1:8} | {2:8} '.format('Apple', '25','40'))
+print('{0:20} | {1:8} | {2:8} '.format('Apple', '25','40'))
+print('{0:20} | {1:8} | {2:8} '.format('Apple', '25','40'))
